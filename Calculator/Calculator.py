d902c559512b29247c9c01aca55fff57391513c2
Benlnj33 <Benlnj33@gmail.com>
2020-02-04 12:06:29 -0500

    added calc functions

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -5,3 +5,12 @@ class Calculator:
 
     def addition(self, a, b):
         return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -1,19 +1,25 @@
 import unittest
 
 from Calculator.Calculator import Calculator
+from CsvReader.CsvReader import CsvReader
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
+        self.assertIsInstance(self.calculator, Calculator)
 
-        self.assertIsInstance(calculator, Calculator)
+    def test_subtraction(self):
+        test_data = CsvReader("Tests/Data/subtraction.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
 
-    def test_calculator_addition(self):
-        calculator = Calculator()
-        result = calculator.addition(1, 2)
-        self.assertEquals(3, result)
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
